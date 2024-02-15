979168917b400dc9940409f9363f0a0a3f98a8e6
Crowdbotics <team@crowdbotics.com>
2024-02-15 07:17:14 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "brilliantsquirrel-46369",
+  url: "https://brilliantsquirrel-46369.botics.co",
+  api: "https://brilliantsquirrel-46369.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
